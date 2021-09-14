beb9b9b53a231cf3c0faab4c5fd1c95c69b6a192
Yi Han <Yi Han>
2021-09-13 21:58:32 -0400

    Initial Commit

---
+++ Exercise One/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise One
+//
+//  Created by 韩熠 on 9/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
